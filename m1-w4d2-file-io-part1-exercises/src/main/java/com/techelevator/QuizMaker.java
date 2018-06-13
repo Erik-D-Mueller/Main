1eed8df72c6a431f949ef8621b2efc5bde2ffbea
Erik Mueller <eriksemailaccount@gmail.com>
2018-06-13 12:49:36 -0400

    Finished Quizmaker

--- m1-w4d2-file-io-part1-exercises/src/main/java/com/techelevator/QuizMaker.java
+++ m1-w4d2-file-io-part1-exercises/src/main/java/com/techelevator/QuizMaker.java
@@ -11,89 +11,130 @@ public class QuizMaker {
 
 	public static void main(String[] args) throws FileNotFoundException {
 		
-		String tempHolder;
-		int previousPipe;
-		int indexOfAnswer;
 		
-		File inputFile = getInputFileFromUser();   // Call method to get file name
+		int numberOfQuestionsAnsweredCorrectly=0;
+			
+		File inputFile = null;   // I have to declare it outside the try-block because otherwise its scope will be limited to the try block
+		
+		do {
+		
+			try{
+				
+					inputFile = getInputFileFromUser();   // This method gets and returns the file name			
+				
+			}
+			
+		
+		catch(Exception e) {
+			
+					System.out.println("File not found, or invalid file name, please try again");		
+					
+					}
+			
+		} while(	!inputFile.canRead()	);
+
 	
-		ArrayList<String> listOfQuestions=new ArrayList<String>();   // At index 0 is the question, above are the answers
+		ArrayList<QuizQuestion> listOfQuestions=new ArrayList<QuizQuestion>(); // This array holds the objects/questions, one object for each question
+		
 		
 		Scanner fileScanner = new Scanner(inputFile);  // This opens the channel to the file	
+		
 		
 		while(fileScanner.hasNextLine()) {  // this loop runs once for each line in the file
 			
 			String line = fileScanner.nextLine();
 		
-			QuizQuestion question= new QuizQuestion();
+			QuizQuestion question = new QuizQuestion();
 			
 			
 			// The below splits the line string into the chunks between the pipes
-			String[] chunks = line.split("|");
-			
-			
-			
-			
+			String[] chunks = line.split("\\|");
 			
 			
 			// This loop finds the index of the string with an asterisk at the end, which is the answer and sets it in the object.
 			// It starts at 1 because the question itself is at index 0
-			for(int i = 1; i< chunks.length; i++) {
 			
+			question.setQuestion( chunks[0] );   // The first part is the question
+			
+			for(int i = 1; i < chunks.length; i++) {
+			
+					
+					// This if statement identifies the answer that has an *, documents that it's the correct answer, then cuts off the *
 					if(chunks[i].endsWith("*")) 
 					{	
-						
-						question.setCorrectAnswerNumber(i);
+						chunks[i] = chunks[i].substring(0, chunks[i].length()-1);  // This cuts off the asterisk
+						question.setCorrectAnswerIndexNumber(i-1);   // This sends the index number of the correct answer
 					}
 				
-	
-				}
-			
-			for(int i = 1; i< chunks.length; i++) {  // This loop loads all the answers from chunks into an answers array, one at a time	
-
 				question.setUpAnswers(chunks[i]);
 			
 			}
 			
-		
+			listOfQuestions.add(question);  // Now that we have everything neatly into the "question" object, we deposit the object into the listOfQuestionsArray
+	
+			
+		}
+			
+			
+			
+/*
+ * So now we have listOfQuestions, an array with an object for each question
+ * 
+ */
 			
 			
 
+
+boolean invalidAnswerFlag = false;
+
+for(int i=0; i<listOfQuestions.size(); i++) {    // This loop iterates once for each question
+	
+	invalidAnswerFlag = false;
+	
+	
+	
+	do {   // This loop runs until they enter a valid answer number, even if it's wrong
+	
+	try{
+	
+		listOfQuestions.get(i).askQuestion();   // This asks the question, which includes all the follow up steps
+	
+	}
+	
+	catch(Exception e) 
+	{
+	System.out.println("You entered an invalid answer, invalid, not simply wrong");	
+	invalidAnswerFlag = true;
+	}
+	
+	}while(invalidAnswerFlag);  // This loop runs until they enter a valid answer anser number, even if it's wrong
+	
+	
+	
+	
+	if(listOfQuestions.get(i).GetUserGaveCorrectAnswer()) { numberOfQuestionsAnsweredCorrectly++; }  // If they got it right, add one to their score
+
+}
+
+// This prints the final statement about their score
+System.out.println("You got " + numberOfQuestionsAnsweredCorrectly + " answer(s) correct out of the total " + listOfQuestions.size() + " questions asked");
+	
 			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-				
-				
-				
-				
-				
-				
-				
 			
 			
 
-		
-		
-			
-			
-			
-			
-		
 	
 		
-		}
+		
 
-	}
+	
+	
+	
+
+
+		
+}	// This is the end of the Main method
+	
 	
 	private static File getInputFileFromUser() {
 		Scanner userInput = new Scanner(System.in);
@@ -101,26 +142,21 @@ public class QuizMaker {
 		String path = userInput.nextLine();
 		
 		File inputFile = new File(path);
-		if(inputFile.exists() == false) { // checks for the existence of a file
-			System.out.println(path+" does not exist");
-			System.exit(1); // Ends the program
-		} else if(inputFile.isFile() == false) {
-			System.out.println(path+" is not a file");
-			System.exit(1); // Ends the program
-		}
+
+// I commented the below out, because it defeats the purpose of my exception handling		
+		
+//		if(inputFile.exists() == false) { // checks for the existence of a file
+//			System.out.println(path+" does not exist");
+//			System.exit(1); // Ends the program
+//		} else if(inputFile.isFile() == false) {
+//			System.out.println(path+" is not a file");
+//			System.exit(1); // Ends the program
+//		}
+	
 		return inputFile;
 	}	
-
-	private static void printQuestionAndAnswers() {
-		
-		
-		
-		
-	}
 	
 	
 	
 	
-	
-	
-}
+} // This is the end of the QuizMaker Class
--- m1-w4d2-file-io-part1-exercises/src/main/java/com/techelevator/QuizQuestion.java
+++ m1-w4d2-file-io-part1-exercises/src/main/java/com/techelevator/QuizQuestion.java
@@ -1,64 +1,106 @@
 package com.techelevator;
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 // This is a class for the quiz questions
 
 public class QuizQuestion {
 
 private String question;
-private String answer1;
-private String answer2;
-private String answer3;
-private String answer4;
-private String answer5;
-int correctAnswerNumber;
-
-ArrayList<String> answers=new ArrayList<String>();
+private boolean userGaveCorrectAnswer = false;  // True if they gave the correct answer
+private int correctAnswerIndexNumber=0;
 
 
-public void setQuestion(String question) {
+ArrayList<String> answers=new ArrayList<String>();   // This array contains all the possible answers for the question and starts at zero, so answer1 is at position zero . . .
+
+
+public void setQuestion(String question) {  //Setter to set the question
 	
 	this.question = question;
 	
 }
 
 
-public void setCorrectAnswerNumber(int correctAnswerIndex) {
-	
-	this.correctAnswerNumber = correctAnswerIndex;
-
-	
-}
-
-
 public void setUpAnswers(String answer) {   // This catches the answers one at a time and adds them to the answers array
 	
-answers.add(answer);
+	answers.add(answer);
 	
 }
 
-public void printEverythingToScreen() {
+public void askQuestion() {
+	
+	printQuestionAndAnswersToScreen();
+	
+	System.out.println();
+	System.out.println("Your answer: ");
+	
+	
+	Scanner userInput = new Scanner(System.in);   //This input is used to get the person's answer
+	
+	String theirAnswerString = userInput.nextLine();
+	
+	
+	// "Does their answer match the "correct answer"?  its -1 because the indexing starts at zero for the answers.
+	// Sets userGaveCorrectAnser to true, if they gave the correct answer
+	
+	
+	
+	
+	
+	if(Integer.parseInt(theirAnswerString) < 1  	|| 	Integer.parseInt(theirAnswerString) > answers.size() ) {   // If the answer number is outside the bounds
+		
+		System.out.println();
+		System.out.println("Invalid Answer!");
+		this.askQuestion();  						 // It recursively calls itself until it gets a valid, albeit possibly wrong answer.
+	}
+	
+	
+	
+	
+	
+	// answers.get(Integer.parseInt(theirAnswerString)).contains(           )
+	
+			
+	userGaveCorrectAnswer = ( 		Integer.parseInt(theirAnswerString) -1 == correctAnswerIndexNumber		);    //If they gave the correct answer, then set it to true
+	
+	if(userGaveCorrectAnswer) {			System.out.println("Correct!");		}
+	else { 		
+
+		System.out.println("Sorry, that isn't correct!"); 		
+		}
+	
+}
+
+public void printQuestionAndAnswersToScreen() {
 	
 // This prints the question	
-System.out.println(question + " ");
-
+	System.out.println();
+	System.out.println(question + " ");
 
 // This prints all the answers
+
+
 for(int i = 0; i< answers.size(); i++) {
-System.out.print(answers.get(i));
+
+System.out.println( (i+1) + ". " + answers.get(i)+ " ");   // The answers index from zero
+
 }
 	
+
+}
+
+public boolean GetUserGaveCorrectAnswer() {
 	
+	return this.userGaveCorrectAnswer;
+}
+
+public void setCorrectAnswerIndexNumber(int correctAnswerIndexNumber){   // The correct answer number is found in the main, then set here
+	
+	this.correctAnswerIndexNumber = correctAnswerIndexNumber;
+		
 }
 
 
-
-
-
-
 }
-
-
-
-
