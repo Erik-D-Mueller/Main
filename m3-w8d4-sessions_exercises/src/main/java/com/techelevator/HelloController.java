ee56e8d7c07afe14460088553540f5bfeecf9c3a
Erik Mueller <eriksemailaccount@gmail.com>
2018-07-14 20:08:17 -0400

    saturday evening

--- m3-w8d4-sessions_exercises/src/main/java/com/techelevator/HelloController.java
+++ m3-w8d4-sessions_exercises/src/main/java/com/techelevator/HelloController.java
@@ -6,12 +6,40 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+
+@SessionAttributes
 @Controller 
 public class HelloController {
 
-	@RequestMapping("/greeting")
-	public String displayGreeting() {
+	@RequestMapping("/Page1")
+	public String displayPage1() {
 		
-		return "greeting";
+		return "Page1";
 	}
+
+
+
+@RequestMapping("/Page2")
+public String displayPage2() {
+	
+	return "Page2";
+}
+
+
+@RequestMapping("/Page3")
+public String displayPage3() {
+	
+	return "Page2";
+}
+
+
+@RequestMapping("/Page4")
+public String displayPage4() {
+	
+	return "Page2";
+}
+
+
+
+
 }
---
+++ m3-w8d4-sessions_exercises/src/main/webapp/WEB-INF/jsp/Page3.jsp
@@ -0,0 +1,33 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title></title>
+	</head>
+	<body>
+	
+	
+		<h1>Favorite Things Exercise</h1>
+		
+			
+		
+		<form method="GET" action=/Page2>
+		
+		
+		<label for="favoriteColor"> </label><h2>What is your favorite color?</h2><input type="text" name="favoriteColor" id="favoriteColor">
+		
+		
+		
+		<br>
+		<br>
+		<input type="submit" value = "Next >>>"	>
+		
+		</FORM>
+		
+		
+		
+		
+		
+	</body>
+</html>
