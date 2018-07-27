a6d28d8fd88decee9b2ae69da4ab66822f964ce9
Erik Mueller <eriksemailaccount@gmail.com>
2018-07-27 10:49:06 -0400

    something changed

--- m4-w10-java-history_geek_exercises/src/main/webapp/js/exercises/event-exerises.js
+++ m4-w10-java-history_geek_exercises/src/main/webapp/js/exercises/event-exerises.js
@@ -1,10 +1,8 @@
 ﻿/// <reference path="../jquery-3.1.1.js" />
-
-
-
+// are JSTL and el included already since we have jquery?
+// or can I not use jstl and jquery since this is not a jsp page?
 
 $(document).ready(function(){
-
 
 	
 var sameAddressBox = $("#SameShipping");
@@ -22,52 +20,97 @@ sameAddressBox.click( function (event) {
 	
 });	
 	
-
-
 $('input:radio[name="ShippingType"]').change(function() {
-	
-
-	
+		
 	var subtotal = parseFloat($("#subtotal").text().slice(11));
 	
-    console.log("7700");
+    
 
-	console.log($(this).datacost);
+	
 
 
 var shipping;
 	switch($(this).val()){
 
 		case "Standard":
+		console.log("33");
 		
-
-
-
-		console.log(data-cost);
 		break;
 		case "Express":
-		console.log("express900");
+		console.log("express44");
 		break;
-		case "Two-day":
-		console.log("Two-day700");
+		case "Two-Day":
+		console.log("Two-day55");
 		break;
 		case "Overnight":
-		console.log("Overnight700");
+		console.log("Overnight66");
 
 	}
 
-
-
-
-	
-	
-	
-	
 });
 	
-	
 
 
+
+
+
+
+
+
+
+
+
+var gameRestartButton=$("#btnRestart");
+gameRestartButton.click(function(event){
+
+	console.log(  "You clicked the restart button!"  );
+
+});
+
+
+// Is it good practice to put these variables out here?
+// Does this stuff out here get loaded?
+var row = 0;
+var column=0;
+
+var gameInput = document.getElementById("game");
+gameInput.addEventListener("keyup", function(event){
+
+	console.log("You keyed up! " + event.key)
+
+if(event.key=="ArrowDown" && row<=8){
+	row++;
+ }
+ if(event.key=="ArrowUp" && row>=1){
+	row--;
+ }
+if(event.key=="LeftArrow" && column>=1){
+	column--;
+}
+if(event.key=="RightArrow" && column<=8){
+	column++;
+}
+
+
+var row = 2;
+var column=3;
+
+location = "#row_" + row + "_column_" + column;
+
+
+$(location).addClass("ship");
+
+
+var table = $("#gameboard")[0];
+
+var cell = table.rows[2].cells[2];
+
+var $cell = $(cell);
+
+$(cell).addClass("ship");
+
+
+});
 
 
 });   // End of ready function
